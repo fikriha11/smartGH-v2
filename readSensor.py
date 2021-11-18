db92ba9fe0e139a093f44883cad82e69458a9a82
fikriha11 <fikrihaikal568@gmail.com>
2021-11-18 07:23:03 +0700

    add sys and sub process

---
+++ readSensor.py
@@ -0,0 +1,4 @@
+from text_to_speech import output
+from time import sleep
+
+output("Selamat datang di greenhous, Suhu Sekrang adalah 33 derajat celcius")
--- text_to_speech.py
+++ text_to_speech.py
@@ -1,10 +1,18 @@
 from typing import Text
 from gtts import gTTS
+import sys
+import subprocess
+subprocess.check_call([sys.executable, '-m', 'pip', 'install','playsound'])
+import playsound
 from playsound import playsound
 
+def output (phrase):
+    my_text = phrase    
+    language = 'id'
+    output = gTTS(text=my_text, lang=language, slow=False)   
+    output.save('temp.mp3')
+    playsound("temp.mp3")
+    return True
 
-my_text = 'Selamat datang di Greenhouse, Suhu Sekarang adalah 23 Derajat Celcius'
-language = 'id'
-output = gTTS(text=my_text, lang=language, slow=False)
-output.save('temp.mp3')
-playsound('temp.mp3')
+
+
