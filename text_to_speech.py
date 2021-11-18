829625cc4d43b3fe03aeb3ef39e1a1369d6db33b
fikriha11 <fikrihaikal568@gmail.com>
2021-11-18 11:57:52 +0700

    py game dengan kondisi

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,11 +1,17 @@
 from gtts import gTTS
-from playsound import playsound
+from pygame import mixer
 
-def output (phrase):   
+def output (phrase):
     language = 'id'
     output = gTTS(text=phrase, lang=language, slow=False)   
     output.save('temp.mp3')
-    playsound('temp.mp3')
+    mixer.init()    
+    mixer.music.load("temp.mp3")
+    mixer.music.set_volume(0.7)
+    mixer.music.play()
+    while mixer.music.get_busy() == True:
+        continue
+    mixer.music.stop()
     return True
 
 
