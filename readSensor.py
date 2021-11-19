22fa97438046f2f2be6ec8c787bc6a933f3be04e
fikriha11 <fikrihaikal568@gmail.com>
2021-11-20 05:55:39 +0700

    add multiThreading

--- readSensor.py
+++ readSensor.py
@@ -2,28 +2,35 @@ import board
 import adafruit_dht
 from time import sleep
 from text_to_speech import output
+from time import thread_time
+from _thread import *
 
 dhtDevice = adafruit_dht.DHT22(board.D23, use_pulseio=False)
-Count = 0
+ThreadCount = 0
+
+def SoundOuput():
+    output(
+    "Selamat datang, Kondisi Suhu ruangan sekarang adalah {} Celcius, dan Kelembapan Sebesar {} Persen".
+    format(
+        temperature_c, humidity
+        )
+    )
+
 while True:
     try:
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
         print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% Count: ".format(
-                temperature_f, temperature_c, humidity, Count
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% Count: {}".format(
+                temperature_f, temperature_c, humidity, ThreadCount
             )
         )
 
-        if Count == 5 :
-            output(
-                "Selamat datang, Kondisi Suhu ruangan sekarang adalah {}, dan Kelembapan Sebesar {} Persen".format(
-                    temperature_c, humidity
-                    )
-                )
-            Count = 0
-        Count += 1
+        if ThreadCount == 5 :
+            start_new_thread(SoundOuput)
+            ThreadCount = 0
+        ThreadCount += 1
  
     except RuntimeError as error:
         print(error.args[0])
