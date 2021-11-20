4930f287e9ead0c84aa26034e1e18f1737220af8
fikriha11 <fikrihaikal568@gmail.com>
2021-11-20 11:41:14 +0700

    force close

--- text_to_speech.py
+++ text_to_speech.py
@@ -2,18 +2,20 @@ from gtts import gTTS
 from pygame import error, mixer
 
 def output (phrase):
-
-    language = 'id'
-    output = gTTS(text=phrase, lang=language, slow=False)   
-    output.save('temp.mp3')
-    mixer.init()    
-    mixer.music.load("temp.mp3")
-    mixer.music.set_volume(1)
-    mixer.music.play()
-    while mixer.music.get_busy() == True:
-        continue
-    mixer.music.stop()
-    return True
+    try :
+        language = 'id'
+        output = gTTS(text=phrase, lang=language, slow=False)   
+        output.save('temp.mp3')
+        mixer.init()    
+        mixer.music.load("temp.mp3")
+        mixer.music.set_volume(1)
+        mixer.music.play()
+        while mixer.music.get_busy() == True:
+            continue
+        mixer.music.stop()
+        return True
+    except Exception as error:
+        print(error)    
     
 
 
