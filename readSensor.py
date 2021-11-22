fc6a2fe5321a0838824eb0392719a017f066b7ae
fikriha11 <fikrihaikal568@gmail.com>
2021-11-22 16:35:10 +0700

    Rubah Code fungsi

--- readSensor.py
+++ readSensor.py
@@ -1,69 +1,71 @@
 import board
+import smbus
 import adafruit_dht
+from gtts import gTTS
+from pydub import AudioSegment
+from pydub.playback import play
 from time import sleep, time
-from text_to_speech import output
-from LuxSensor import readLux
 
-# millis
-dhtTime = time()
-luxTime = time()
-printTime = time()
-dhtDevice = adafruit_dht.DHT22(board.D23, use_pulseio=False)
-Count = 0
-
-# var Sensor
-temperature_c = 0
-humidity = 0
-temperature_f = 0
-LuxValue = 0
-
-# boolean
-Sound = True
+SoundTime = time()
+dhtDevice = adafruit_dht.DHT22(board.D25, use_pulseio=False)
 
 
-def SoundOuput():
-    output(
-        "Selamat datang, Kondisi Suhu ruangan sekarang adalah {} derajat Celcius, dan Kelembapan Sebesar {} Persen.".
-        format(
-            temperature_c, humidity
-        ) + "Untuk Keterangan Cahaya, adalah Sebesar {} Lumen, Terima Kasih".format(LuxValue)
-    )
+def soundOutput():
+    try:
+        phrase = f"Selamat datang, Kondisi Suhu ruangan sekarang adalah {round(temperature_c)} derajat Celcius, dan Kelembapan udara mencapai {round(humidity)} Persen."
+        phrase1 = f"Untuk Keterangan Cahaya Sebesar {lux} Lumen, Terima Kasih"
+        language = 'id'
+        output = gTTS(text=phrase + phrase1, lang=language, slow=False)
+        output.save('temp.wav')
+        song = AudioSegment.from_mp3('temp.wav')
+        play(song)
+        return True
+    except Exception as error:
+        print("SoundOutput Error")
+
+
+def readLux():
+    global lux
+    try:
+        bus = smbus.SMBus(1)
+        bus.write_byte_data(0x39, 0x00 | 0x80, 0x03)
+        bus.write_byte_data(0x39, 0x01 | 0x80, 0x02)
+        sleep(0.5)
+        data = bus.read_i2c_block_data(0x39, 0x0C | 0x80, 2)
+        lux = data[1] * 256 + data[0]
+        return True
+    except Exception as error:
+        print("Lux data error")
+
+
+def readDHT():
+    global temperature_c, temperature_f, humidity
+    try:
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        return True
+    except Exception as error:
+        print("Sensor DHT error")
+
+
+def ReadSensor():
+    try:
+        readDHT()
+        readLux()
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}%".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
+        print(f"Lux Meter : {lux} lux")
+        return True
+    except Exception as error:
+        print("Print Error")
 
 
 while True:
-    try:
-        if(time() - dhtTime) > 2:
-            temperature_c = dhtDevice.temperature
-            temperature_f = temperature_c * (9 / 5) + 32
-            humidity = dhtDevice.humidity
-            dhtTime = time()
-
-        if(time() - luxTime) > 5:
-
-            LuxValue = readLux()
-            luxTime = time()
-
-        if(time() - printTime) >= 1:
-
-            print(
-                "Temp: {:.1f} F / {:.1f} C    Humidity: {}% Count: {}".format(
-                    temperature_f, temperature_c, humidity, Count
-                )
-            )
-            print(f"Lux Meter : {LuxValue} lux")
-            printTime = time()
-
-        if Count == 10:
-            SoundOuput()
-            Count = 0
-
-        Count += 1
-        sleep(1)
-
-    except RuntimeError as error:
-        # print(error.args[0])
-        sleep(2.0)
-        continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
+    ReadSensor()
+    if(time() - SoundTime) > 30:
+        soundOutput()
+        SoundTime = time()
