47e48063e6511e4ce8d73d66f7a12c6a7e7200e4
fikriha11 <fikrihaikal568@gmail.com>
2021-11-29 03:47:53 +0700

    button pull up

--- button.py
+++ button.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 ButtonPin = 23
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(ButtonPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(ButtonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     print(GPIO.input(ButtonPin))
