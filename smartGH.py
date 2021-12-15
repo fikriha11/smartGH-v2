40805350a25fb90d270cd4b1404446c53d2d75d5
fikriha11 <fikrihaikal568@gmail.com>
2021-12-16 05:33:56 +0700

    Firmware Update

--- smartGH.py
+++ smartGH.py
@@ -13,7 +13,6 @@ import RPi.GPIO as GPIO
 from datetime import datetime as dt
 from gtts import gTTS
 
-
 url = "https://hidroponikwirolegi.belajarobot.com/sensor/insert"
 api_key = "a1ffqsVcx45IuG"
 
